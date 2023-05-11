cf7c812d305dc1647ceca719fde69f0d76b3adaa
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-05-11 20:29:50 +0200

    full manual merge front and back

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,11 +3,12 @@ import Navbar from './components/Navbar';
 import CreateUser from './components/CreateUser';
 import Login from './components/Login';
 import Luggage from './pages/Luggage';
+import UserProfile from './pages/UserProfile';
 function App() {
   return (
     <>
       <Navbar />
-      <Luggage />
+      <UserProfile />
     </>
   );
 }
