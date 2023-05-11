b390c61345a48919a663a2de731fe3f4f0c26065
axel castro <castroaxel.f@gmail.com>
2023-05-11 18:12:11 +0200

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import './App.css';
 import Navbar from './components/Navbar';
 import CreateUser from './components/CreateUser';
 import Login from './components/Login';
-
+import Luggage from './pages/Luggage'
 function App() {
   return (
     <>
     <Navbar />
-    <Login/>
+    <Luggage />
     </>
   );
 }
--- src/pages/Luggage.jsx
+++ src/pages/Luggage.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import Valise from '../assets/img/Luggage.png';
+import Navbar from '../components/Navbar'
+import Home from '../assets/img/Home-bg.png';
+import './Luggage.css';
 
 const Luggage = () => {
-  return <div>Luggage</div>;
-};
+  return (
+  <div className='home'>
+    <div className='background'>
+      <img src={Home} alt="mapmonde" />
+    </div>
+    <div className='luggage'>
+      <img src={Valise} alt="valise" />
+    </div>
+    <button className='play' type="submit">Faire ma valise</button>
+  </div>
+)};
 
 export default Luggage;
