1cd1ce6de6d86b09f2902e33d342086a88ff2ee7
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-05-11 22:32:01 +0200

    local api call OK with cors option

--- back/index.js
+++ back/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const EXPRESS_PORT = 3030;
 const countriesRouter = require('./src/routes/countries');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*'
+}
+app.use(cors(corsOptions));
+
+app.use('/api/countries', countriesRouter);
 
 app.use(express.json());
-app.use('/api/countries', countriesRouter);
 
 app.listen(EXPRESS_PORT, () =>
   console.log('Server is running on: %s', EXPRESS_PORT)
--- front/src/pages/Luggage.jsx
+++ front/src/pages/Luggage.jsx
@@ -1,9 +1,20 @@
 import Valise from '../assets/img/Luggage.png';
 import Navbar from '../components/Navbar'
 import Home from '../assets/img/Home-bg.png';
+import axios from 'axios';
 import './Luggage.css';
+import { useEffect, useState } from 'react';
 
 const Luggage = () => {
+
+    const[country, setCountry] = useState({})
+
+
+    useEffect(()=>{
+        axios.get('http://localhost:3030/api/countries')
+            .then((res) => setCountry(res.data) )
+    },[])
+
   return (
   <div className='home'>
     <div className='background'>
