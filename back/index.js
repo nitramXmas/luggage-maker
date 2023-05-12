ae8c14b3128e3aa2e76a01e9304e1cb9a1e3b456
Rom-Val <126482440+Rom-Val@users.noreply.github.com>
2023-05-12 14:58:58 +0200

    Update index.js

--- back/index.js
+++ back/index.js
@@ -1,20 +1,21 @@
-const express = require('express');
 const app = express();
 const EXPRESS_PORT = 3030;
 const countriesRouter = require('./src/routes/countries');
+const objectsRouter = require('./src/routes/objects');
+const luggageRouter = require('./src/routes/luggage');
 const userRouter = require('./src/routes/user')
 const cors = require('cors');
 
 const corsOptions = {
+    origin: '*'
   origin: '*'
 }
 app.use(cors(corsOptions));
-
-app.use('/api/countries', countriesRouter);
-app.use('/api/users', userRouter);
-
 app.use(express.json());
 
-app.listen(EXPRESS_PORT, () =>
-  console.log('Server is running on: %s', EXPRESS_PORT)
-);
+app.use('/api/countries', countriesRouter);
+app.use('/api/objects', objectsRouter )
+app.use('/api/luggage', luggageRouter)
+app.use('/api/users', userRouter);
+
+
