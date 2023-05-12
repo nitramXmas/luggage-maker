4601518a3bd0ebe677f7ed4f0bb0d68fcf416762
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-05-12 03:07:47 +0200

    luggage positionning

--- front/src/pages/Game.jsx
+++ front/src/pages/Game.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Card from '../components/Card'
+import luggage from '../assets/img/valise.png'
 
 import './Game.css'
 
@@ -13,12 +14,12 @@ const Game = () => {
             .then((res) => setCountry(res.data))
     },[])
   return (
-    <div className='game'>
+    <div className='game' style={{backgroundImage: `url(${country.img})`}}>
         <Card {...country} />
-        <img className='bg-image' src={country.img} alt="" />
+        <img className='luggage'  src={luggage} alt="" />
         <div className='buttons'>
-            <Link to='/'><button className='reset' type="submit" >RESET</button></Link>
-            <Link to='/result'><button className='submitLuggage' type="submit" >SUBMIT</button></Link>
+            <Link to='/'><button className='play' type="submit" >RESET</button></Link>
+            <Link to='/result'><button className='play' type="submit" >SUBMIT</button></Link>
         </div>
     </div>
   )
